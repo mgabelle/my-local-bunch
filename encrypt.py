84c5217e6546425c34bc033005baeb765275e162
mgabelle <gabellemathieu18@gmail.com>
2020-02-27 15:13:31 +0100

    Beginning of encryption
    -Small test to encrypt
    
    --> Must store the salt
    --> create functions for encryption
    --> function for decryption
    --> another module for json/data management

--- encrypt.py
+++ encrypt.py
@@ -1,4 +1,30 @@
+# External Imports
+import os
+import base64
+
 from cryptography.fernet import Fernet
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-f = Fernet(Fernet.generate_key())
 
+# Open file with data
+with open("data.txt","rb") as f:
+    password = f.read()
+
+salt = os.urandom(16)
+
+kdf = PBKDF2HMAC(
+    algorithm=hashes.SHA256(),
+    length=32,
+    salt=salt,
+    iterations=10**5,
+    backend=default_backend()
+)
+
+key = base64.urlsafe_b64encode(kdf.derive(password))
+
+f = Fernet(key)
+
+text = input("enter text : ").encode()
